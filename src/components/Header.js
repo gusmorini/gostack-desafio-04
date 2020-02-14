25c2df6e73ee6f3d7d5421d6adc338a5f34414fd
Gustavo Morini <gustavommh@gmail.com>
2020-02-14 08:57:14 -0300

    logo facebook, icon user profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+const iconUser = <FontAwesomeIcon icon={faUserCircle} />;
+
+
 import logoFacebook from '../assets/facebook-1.png';
 import logoUser from '../assets/user.png';
 
@@ -8,13 +14,14 @@ function Header() {
         <header>
             <div className="container">
                 <nav className="menu">
-                    <img
-                        src={logoFacebook}
-                        className='logo'
-                        alt="logo facebook" />
-                    <a className='profile'>
-                        <span>Meu perfil</span>
-                        <img src={logoUser} alt="profile" />
+                    <a href="#" className="logo">
+                        <img
+                            src={logoFacebook}
+                            alt="logo facebook" />
+                    </a>
+                    <a className='profile' href="#">
+                        Meu perfil
+                        <span>{iconUser} </span>
                     </a>
                 </nav>
             </div>
