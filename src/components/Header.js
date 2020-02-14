b0d28861cbb7bad927082e61c1099fd199435d17
Gustavo Morini <gustavommh@gmail.com>
2020-02-13 22:19:33 -0300

    user profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import logoFacebook from '../assets/facebook-1.png';
+import logoUser from '../assets/user.png';
 
 function Header() {
     return (
@@ -11,6 +12,10 @@ function Header() {
                         src={logoFacebook}
                         className='logo'
                         alt="logo facebook" />
+                    <a className='profile'>
+                        <span>Meu perfil</span>
+                        <img src={logoUser} alt="profile" />
+                    </a>
                 </nav>
             </div>
         </header>
